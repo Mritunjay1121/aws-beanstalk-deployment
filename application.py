ceb8d60839c7d8fa544fd3ae19e8860c1fdaa950
Mritunjay1121 <mritunjay.1121@gmail.com>
2025-03-26 02:40:46 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+application=Flask(__name__)
+app=application
+
+@app.route("/",methods=["GET","POST"])
+def run():
+    return "Hello"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")

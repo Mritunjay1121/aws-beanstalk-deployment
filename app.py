7dbcfcbb05cad87066d3fdc89bc829bd68cb8a67
Mritunjay1121 <mritunjay.1121@gmail.com>
2025-03-27 17:20:35 +0530

    added for ec2 gradio

--- app.py
+++ app.py
@@ -1,31 +1,8 @@
 import gradio as gr
 
-# Define a simple function
-def say_hello():
-    return "Hello from Gradio!"
+def greet(name):
+    return "Hello " + name + "!"
 
-# Create the Gradio interface
-# demo = gr.Interface(
-#     fn=say_hello,       # The function to execute
-#     inputs=None,        # No inputs required
-#     outputs="text",     # Output will be text
-#     title="Simple Hello App",
-#     description="A basic Gradio app that says Hello!"
-# )
-
-
-gr.Interface(
-    fn=say_hello,       # The function to execute
-    inputs=None,        # No inputs required
-    outputs="text",     # Output will be text
-    title="Simple Hello App",
-    description="A basic Gradio app that says Hello!",
-    server_name="0.0.0.0"
-).launch()
-# Launch the app
-# if __name__ == "__main__":
-#     demo.launch(
-#         server_name="0.0.0.0",  # Localhost
-#         server_port=8000,        # Default Gradio port (changed from 8080 to avoid conflicts)
-#         debug=True               # Enable debug mode for troubleshooting
-#     )
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    
+demo.launch(server_name="0.0.0.0")
